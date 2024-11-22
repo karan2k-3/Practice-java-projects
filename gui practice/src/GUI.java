1dc6c1baaf37e4a87cce757bcce7f80ef95c68fc
karan2k-3 <kkarann367@gmail.com>
2024-11-23 01:05:06 +1100

    kinda the basic structure of my calculator

--- gui practice/src/GUI.java
+++ gui practice/src/GUI.java
@@ -1,8 +1,10 @@
 import java.awt.FlowLayout;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+
 
 public class GUI {
     public static void main(String[] args) {
@@ -11,13 +13,71 @@ public class GUI {
     }
     
 }
-class abc extends JFrame{//follows crd layout can only see the top card of the stack
+class abc extends JFrame{//follows card layout can only see the top card of the stack
 
+    JTextField t1,t2;//adding text feilds to my empty frame later gonna modify 
+    JButton b,minus,multiply,divide;
+    public abc(){
+    t1 = new JTextField(7);
+    t2 = new JTextField(7);
+    b = new JButton("+");
+    minus = new JButton("-");
+    multiply = new JButton("*");
+    divide = new JButton("/");
+
+    add(t1);
+    add(t2);
+    add(b);
+    add(minus);
+    add(multiply);
+    add(divide);
+    ActionListener act = new ActionListener() {
+        public void actionPerformed(ActionEvent ae) {
+            int num1= Integer.parseInt(t1.getText());//integer.parsint converets string value to integer
+                int num2=Integer.parseInt(t2.getText());
+                int value = num1 + num2;
+                b.setText(value + "");//any interger output + "" is a string output
+            
+        }
+    };
+    b.addActionListener(act);
+    ActionListener ac = new ActionListener() {
+        public void actionPerformed(ActionEvent ae) {
+            int num1= Integer.parseInt(t1.getText());//integer.parsint converets string value to integer
+                int num2=Integer.parseInt(t2.getText());
+                int value = num1 - num2;
+                minus.setText(value + "");//any interger output + "" is a string output
+            
+        }
+    };
+    minus.addActionListener(ac);
+    ActionListener act1 = new ActionListener() {
+        public void actionPerformed(ActionEvent ae) {
+            int num1= Integer.parseInt(t1.getText());//integer.parsint converets string value to integer
+                int num2=Integer.parseInt(t2.getText());
+                int value = num1 * num2;
+                multiply.setText(value + "");//any interger output + "" is a string output
+            
+        }
+    };
+    multiply.addActionListener(act1);
+
+    ActionListener act2 = new ActionListener() {
+        public void actionPerformed(ActionEvent ae) {
+            int num1= Integer.parseInt(t1.getText());//integer.parsint converets string value to integer
+                int num2=Integer.parseInt(t2.getText());
+                int value = num1 /num2;
+                divide.setText(value + "");//any interger output + "" is a string output
+            
+        }
+    };
+    divide.addActionListener(act2);
+    
     ///this whole section is basic for creating a jframe window 
-   public abc(){
+   
     setLayout(new FlowLayout());
     setVisible(true);
-    setSize(400,400);
+    setSize(500,500);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//this is used to close jframe code not just making it invisible
    }
 }
--- gui practice/src/addgui.java
+++ gui practice/src/addgui.java
@@ -2,10 +2,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 public class addgui {
     public static void main(String[] args) {
